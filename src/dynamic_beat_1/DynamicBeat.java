0e23dac181fc08b8437c207f043f43b89532405c
jueyoung jung <wjdwndudghlw@naver.com>
2024-05-22 11:14:23 +0900

    2-이미지업로드완료

--- src/dynamic_beat_1/DynamicBeat.java
+++ src/dynamic_beat_1/DynamicBeat.java
@@ -2,15 +2,18 @@ package dynamic_beat_1;
 import javax.swing.JFrame;
 
 
+
 public class DynamicBeat extends JFrame {
-    
-    public DynamicBeat(){
+
+
+    public DynamicBeat() {
         setTitle("Dynamic Beat");
-        setSize(Main.SCREENT_WIDTH,Main.SCREENT_HEIGHT);
+        setSize(dynamic_beat_1.Main.SCREENT_WIDTH, dynamic_beat_1.Main.SCREENT_HEIGHT);
         setResizable(false); // 인위적으로 줄이기 X
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        
+
     }
+
 }
