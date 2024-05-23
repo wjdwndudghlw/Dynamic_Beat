d6e271d1c7a6895b0a1584b6633ee83c12915c64
jueyoung jung <wjdwndudghlw@naver.com>
2024-05-23 11:48:51 +0900

    8-게임난이도버튼구현완료

---
+++ src/dynamic_beat_8/Main.java
@@ -0,0 +1,14 @@
+package dynamic_beat_8;
+
+
+public class Main {
+
+    // 상수는 대문자로 표기
+    public static final int SCREENT_WIDTH = 1280;
+    public static final int SCREENT_HEIGHT = 720;
+
+    public static void main(String[] args) {
+        new DynamicBeat();
+        
+    }
+}
