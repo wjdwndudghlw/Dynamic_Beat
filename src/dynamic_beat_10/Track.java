0d81b0abfc147542fb289bab1c17b38b4d1b8d90
jueyoung jung <wjdwndudghlw@naver.com>
2024-05-24 01:39:11 +0900

    10-게임화면구현완료

---
+++ src/dynamic_beat_10/Track.java
@@ -0,0 +1,63 @@
+package dynamic_beat_10;
+
+public class Track {
+
+	private String titleImage; 
+	private String startImage;
+	private String gameImage; 
+	private String startMusic; 
+	private String gameMusic; 
+	
+
+	public String getTitleImage() {
+		return this.titleImage;
+	}
+
+	public void setTitleImage(String titleImage) {
+		this.titleImage = titleImage;
+	}
+
+	public String getStartImage() {
+		return this.startImage;
+	}
+
+	public void setStartImage(String startImage) {
+		this.startImage = startImage;
+	}
+
+	public String getGameImage() {
+		return this.gameImage;
+	}
+
+	public void setGameImage(String gameImage) {
+		this.gameImage = gameImage;
+	}
+
+	public String getStartMusic() {
+		return this.startMusic;
+	}
+
+	public void setStartMusic(String startMusic) {
+		this.startMusic = startMusic;
+	}
+
+	public String getGameMusic() {
+		return this.gameMusic;
+	}
+
+	public void setGameMusic(String gameMusic) {
+		this.gameMusic = gameMusic;
+	}
+
+	public Track(String titleImage, String startImage, String gameImage, String startMusic, String gameMusic) {
+		super();
+		this.titleImage = titleImage;
+		this.startImage = startImage;
+		this.gameImage = gameImage;
+		this.startMusic = startMusic;
+		this.gameMusic = gameMusic;
+	}
+
+
+	
+}
